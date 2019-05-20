1a579be3c0150ab83563e310a6c4c63b5c6a6f0c
matt venn <matt@dygma.com>
2019-05-20 20:57:18 +0200

    show ip

--- destroy.py
+++ destroy.py
@@ -5,7 +5,7 @@ mattvenn = 16303456
 manager = digitalocean.Manager(token=token)
 my_droplets = manager.get_all_droplets(tag_name="moviepy")
 for droplet in my_droplets:
-    print(droplet)
+    print(droplet, droplet.ip_address)
     if droplet.id == mattvenn:
         print("somehow got mattvenn droplet!")
         exit(1)
