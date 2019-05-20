b4fbd5603747c7e0dc5867d550296f728f9944d2
matt venn <matt@dygma.com>
2019-05-20 19:50:49 +0200

    show ips

--- list.py
+++ list.py
@@ -3,4 +3,5 @@ import digitalocean
 from tokens import token
 manager = digitalocean.Manager(token=token)
 my_droplets = manager.get_all_droplets()
-print(my_droplets)
+for d in my_droplets:
+    print(d, d.ip_address)
